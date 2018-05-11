a9db5958f3acfc1558925ffd6006f4b1d130b51e
Kshitij Tamrakar <31537287+kshitij41@users.noreply.github.com>
2018-05-11 21:46:59 +0530

    Add files via upload

---
+++ apples2.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class apples2 {
+	public static void main(String[] args){
+		int[] ar1 = {2,18,1,42,5,23,5,31,6};
+		projects x = new projects();
+		x.EvenBeforeOdd(ar1);
+		projects lessmorek = new projects();
+		lessmorek.kSort(ar1, 32);
+		projects sumK = new projects();
+		sumK.sumK(ar1, 41);
+		
+		List<Integer> list1 = new ArrayList<Integer>();
+		list1.add(5);
+		list1.add(34);
+		list1.add(24);
+		Collections.rotate(list1, 1);
+		System.out.println(list1.get(0));
+		
+		Stack<Integer> st = new Stack<Integer>();
+		int[] arrForstack = {3,42,1,3,12,31};
+		for(int i=0;i<arrForstack.length;i++) {st.add(arrForstack[i]);}
+		Stack<Integer> st2 = new Stack<Integer>();
+		while(st.iterator().hasNext()) {st2.add(st.pop());}
+		Iterator<Integer> iter = st2.iterator();
+		for(int i =0;i<arrForstack.length;i++) {
+			System.out.print(iter.next()+" ");
+		 }
+		
+		
+		Deque<Integer> de = new ArrayDeque<Integer>();
+		int[] toAdd = {1,2,3,4,5,6,7,8};
+		for(int i=0;i<toAdd.length;i++) {de.add(toAdd[i]);}
+		Queue<Integer> qe = new LinkedList<Integer>();
+		Iterator<Integer> deqIter = de.iterator();
+		for(int i =0; i <3;i++) {qe.add(de.removeFirst());}
+		int tempDeq = de.removeFirst();
+		qe.add(de.removeFirst());
+		qe.add(tempDeq);
+		while(!(de.isEmpty())) {qe.add(de.removeFirst());}
+		while(!(qe.isEmpty())) {de.add(qe.remove());}
+		System.out.println();
+		System.out.println(de.toString());
+		
+		
+		projects StackT = new projects();
+		Stack<Integer> st1 = new Stack<Integer>();
+		st1.add(3);st1.add(4);st1.add(45);st1.add(23);
+		StackT.StackTransfer(st1);
+		
+		String sss = "they there";
+		System.out.println(sss.toCharArray()[5]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+
+}
